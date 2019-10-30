bfe62cbbb8a05835b78fae51fcd1f4f056bbc7f2
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2019-10-30 09:08:15 +0100

    basic app

--- src/app.js
+++ src/app.js
@@ -9,11 +9,8 @@ var methodOverride = require('method-override');
 var flash = require("connect-flash");
 var request = require("request");
 
-
-
-
 mongoose.Promise = global.Promise;
-mongoose.connect(DB_URL, {
+mongoose.connect('mongodb://localhost:27017/marketplace', {
     useNewUrlParser: true, 
     useFindAndModify: false, 
     useCreateIndex:true,
@@ -29,31 +26,31 @@ app.use(methodOverride("_method"));
 
 
 //PASSPORT CoNFIG
-app.use(require("express-session")({
-    secret: SECRET_KEY,
-    resave: false,
-    saveUninitialized: false, 
-    expires: new Date(Date.now() + (30 * 86400 * 1000))
-}));
+// app.use(require("express-session")({
+//     secret: SECRET_KEY,
+//     resave: false,
+//     saveUninitialized: false, 
+//     expires: new Date(Date.now() + (30 * 86400 * 1000))
+// }));
 
-passport.use("local", new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.use("local", new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
 
-passport.use("admin-local", new LocalStrategy(Admin.authenticate()));
-passport.serializeUser(Admin.serializeUser());
-passport.deserializeUser(Admin.deserializeUser());
+// passport.use("admin-local", new LocalStrategy(Admin.authenticate()));
+// passport.serializeUser(Admin.serializeUser());
+// passport.deserializeUser(Admin.deserializeUser());
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    next();
-})
+// app.use(function(req, res, next){
+//     res.locals.currentUser = req.user;
+//     next();
+// })
 
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(PORT, ()=>{
+app.listen(PORT, process.env.IP, ()=>{
     console.log(`Listening on ${PORT}`)
 })
