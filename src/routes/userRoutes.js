fae5a81d478be376de54bba9a323f5b75fef5129
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2019-11-05 19:48:56 +0100

    User can create a service

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,13 @@
 import {newUser,registerUser,editUser, loginUser,logoutUser,isLoggedIn} from "../middlewares/middlewares";
 import { logHelp } from "../helpers/login";
+import { renderServicePage, createService } from "../middlewares/createService";
 
 export const initRoutes = app =>{
     app.get("/", (req,res)=>res.render("index"));
     app.get("/user/register", (req,res)=>res.render("register"));
     app.get("/user/login", (req,res)=>res.render("login"));
     app.get("/user/dashboard", isLoggedIn, (req,res)=>res.render("dashboard"));
-    app.get("/user/:id/services/new", (req,res)=>res.render("createService"))
+    app.get("/user/:id/services/new", isLoggedIn, renderServicePage)
     app.get("/user/login/success", (req,res)=>res.send(`${req.user} Logged in successfully!`))
     app.post("/user/register", registerUser);
     app.post("/user/login", loginUser, logHelp);
@@ -15,6 +16,6 @@ export const initRoutes = app =>{
 // Update User details
     app.put("/user/:id", editUser)
 // Create Service
-    app.post("/user/:id/services/new")
+    app.post("/user/:id/services/new", createService)
    
 }
