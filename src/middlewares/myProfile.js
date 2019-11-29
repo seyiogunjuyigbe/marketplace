b3d12191d899f233c8ad055243245cdebf4aac2c
Oluwaseyi Ogunjuyigbe <seyiogunjuyigbe@gmail.com>
2019-11-29 06:18:40 +0100

    service sort

--- src/middlewares/myProfile.js
+++ src/middlewares/myProfile.js
@@ -1,5 +1,5 @@
 const User = require ("../models/user");
-
+const Service = require ("../models/service")
 
 export const myProfile = (req,res,next) =>{
            User.findById(req.user._id, (err,user)=>{
@@ -7,7 +7,12 @@ export const myProfile = (req,res,next) =>{
                     return res.status(400).render("error", {errMessage: err.message})
                }
                else{
-                return res.status(200).render("dashboard", {user:user})
+                   Service.find({createdBy:user._id}, (err,services)=>{
+                       if(!err){
+                        return res.status(200).render("dashboard", {user:user, services:services})
+
+                       }
+                   })
 
                }
         })
